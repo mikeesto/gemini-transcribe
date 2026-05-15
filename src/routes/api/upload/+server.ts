0c86760a905a03b8fbc62e5e72a7eeb4e0656447
Michael Esteban <mickel13@gmail.com>
2026-05-15 16:19:13 +1000

    update model gemini-3.1-flash-lite-preview to gemini-3.1-flash-lite

--- src/routes/api/upload/+server.ts
+++ src/routes/api/upload/+server.ts
@@ -400,7 +400,7 @@ export async function POST(event) {
 				const models = [
 					'gemini-3-flash-preview',
 					'gemini-2.5-flash',
-					'gemini-3.1-flash-lite-preview',
+					'gemini-3.1-flash-lite',
 					'gemini-2.5-flash-lite'
 				];
 
